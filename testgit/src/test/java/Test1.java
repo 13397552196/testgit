645589a4fe2ef6de85b3bff52d1c2c9193989a48
hq <hq2639129255.com>
2022-01-13 18:52:39 +0800

    创建分支dev

---
+++ testgit/src/test/java/Test1.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+public class Test1 {
+    @Test
+public  void  test(){
+    System.out.println("ok1");
+    System.out.println("ok2");
+    System.out.println("ok2");
+        System.out.println("ok4");
+        System.out.println("分支创建");
+}
+}
+
